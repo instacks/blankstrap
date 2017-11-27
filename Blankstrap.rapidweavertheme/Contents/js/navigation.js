cd9e2339d3e4a52448466f97684542e9a01c2d2b
Jannis Rondorf <github@jrondorf.de>
2017-11-27 18:01:10 +0100

    add bootstrap 4 native second level navigation

--- Blankstrap.rapidweavertheme/Contents/js/navigation.js
+++ Blankstrap.rapidweavertheme/Contents/js/navigation.js
@@ -1,3 +1,8 @@
 $(function() {
     $("#blankstrap-nav").css("display", "flex");
+    $("#navbarSupportedContent>ul").addClass("navbar-nav").addClass("mr-auto");
+    $(".navbar-nav").children(".dropdown").children("a").addClass("dropdown-toggle").attr("href", "#").attr("data-toggle", "dropdown").attr("aria-haspopup", "true").attr("aria-expanded", "false");
+    $(".navbar-nav").children(".dropdown").children("ul").addClass("dropdown-menu").attr("aria-labelledby", "navbarDropdownMenuLink");
+    $(".navbar-nav").children(".dropdown").children("ul").children("li").children("a").removeClass("nav-link").addClass("dropdown-item");
+    $(".navbar-nav").children(".dropdown").children("ul").children("li").children("ul").addClass("blankstrap-hide");
 });
