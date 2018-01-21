18b7c9c80c552161e165999276c4bff87d22e125
Jannis Rondorf <jannis1010@yahoo.de>
2018-01-21 22:56:06 +0100

    update to bs4 stable #16

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# default bootstrap
+cp ../bootstrap/dist/js/bootstrap.bundle.min.js Blankstrap.rapidweavertheme/Contents/js/bootstrap.bundle.min.js
+cp ../bootstrap/dist/css/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/bootstrap.min.css
+# bootswatch
+cp ../bootswatch/dist/cerulean/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/cerulean/bootstrap.min.css
+cp ../bootswatch/dist/cosmo/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/cosmo/bootstrap.min.css
+cp ../bootswatch/dist/cyborg/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/cyborg/bootstrap.min.css
+cp ../bootswatch/dist/darkly/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/darkly/bootstrap.min.css
+cp ../bootswatch/dist/flatly/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/flatly/bootstrap.min.css
+cp ../bootswatch/dist/journal/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/journal/bootstrap.min.css
+cp ../bootswatch/dist/litera/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/litera/bootstrap.min.css
+cp ../bootswatch/dist/lumen/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/lumen/bootstrap.min.css
+cp ../bootswatch/dist/lux/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/lux/bootstrap.min.css
+cp ../bootswatch/dist/materia/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/materia/bootstrap.min.css
+cp ../bootswatch/dist/minty/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/minty/bootstrap.min.css
+cp ../bootswatch/dist/pulse/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/pulse/bootstrap.min.css
+cp ../bootswatch/dist/sandstone/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/sandstone/bootstrap.min.css
+cp ../bootswatch/dist/simplex/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/simplex/bootstrap.min.css
+cp ../bootswatch/dist/sketchy/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/sketchy/bootstrap.min.css
+cp ../bootswatch/dist/slate/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/slate/bootstrap.min.css
+cp ../bootswatch/dist/solar/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/solar/bootstrap.min.css
+cp ../bootswatch/dist/spacelab/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/spacelab/bootstrap.min.css
+cp ../bootswatch/dist/superhero/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/superhero/bootstrap.min.css
+cp ../bootswatch/dist/united/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/united/bootstrap.min.css
+cp ../bootswatch/dist/yeti/bootstrap.min.css Blankstrap.rapidweavertheme/Contents/css/yeti/bootstrap.min.css
