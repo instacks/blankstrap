53b8655b1acccf23c78406800e1fbea66dc13291
Jannis Rondorf <github@jrondorf.de>
2021-06-23 15:46:38 +0200

    5.0.2

---
+++ build.sh
@@ -0,0 +1,4 @@
+cp node_modules/bootstrap/dist/css/bootstrap.min.css Blankstrap5.stack/core.stack/Contents/Resources/blankstrap5/css 
+cp node_modules/bootstrap/dist/css/bootstrap.min.css.map Blankstrap5.stack/core.stack/Contents/Resources/blankstrap5/css
+cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js Blankstrap5.stack/core.stack/Contents/Resources/blankstrap5/js 
+cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map Blankstrap5.stack/core.stack/Contents/Resources/blankstrap5/js
