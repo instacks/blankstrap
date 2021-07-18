bc9edb70734287bafbffb8a21beedaf4ce9e385e
Jannis Rondorf <github@jrondorf.de>
2021-07-18 21:03:14 +0200

    ids

--- Blankstrap5.stack/slide.stack/Contents/Resources/page.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.carousel-inner').forEach(function(element){
-        element.children[0].className = element.children[0].className + ' active'
-    });
-});
