a5ffb609d89cb83daabe82e08a383a737672222b
Jannis Rondorf <github@jrondorf.de>
2021-08-13 11:14:26 +0200

    docs

---
+++ docs/sidebar-navigation/files/stacks_page_page10.js
@@ -0,0 +1,7 @@
+
+var stacks = {};
+stacks.stacks_in_44 = {};
+stacks.stacks_in_44 = (function(stack) {document.addEventListener('DOMContentLoaded', function () {
+    blankstrap.initializeNavigation('#stacks_in_44', 'fixed-headroom');
+});
+return stack;})(stacks.stacks_in_44);
---
+++ docs/styling/files/stacks_page_page18.js
@@ -0,0 +1,7 @@
+
+var stacks = {};
+stacks.stacks_in_17_6 = {};
+stacks.stacks_in_17_6 = (function(stack) {document.addEventListener('DOMContentLoaded', function () {
+    blankstrap.initializeNavigation('#testnav', 'fixed-headroom');
+});
+return stack;})(stacks.stacks_in_17_6);
