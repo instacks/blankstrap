f928e0222d2b2deeccfa62e99736daffc215c30d
Jannis Rondorf <github@jrondorf.de>
2023-06-06 22:53:49 +0200

    5.3.0

---
+++ docs/form/files/stacks_page_page12.js
@@ -0,0 +1,7 @@
+
+var stacks = {};
+stacks.stacks_in_7_6 = {};
+stacks.stacks_in_7_6 = (function(stack) {document.addEventListener('DOMContentLoaded', function () {
+    blankstrap.initializeNavigation('#testnav', 'fixed-headroom');
+});
+return stack;})(stacks.stacks_in_7_6);
