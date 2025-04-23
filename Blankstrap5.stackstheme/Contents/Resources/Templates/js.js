c73915d8e574bf320a6198818eedbb17ccb9e200
jrondorf <github@jrondorf.de>
2025-04-23 22:30:44 +0200

    add stacks theme

---
+++ Blankstrap5.stackstheme/Contents/Resources/Templates/js.js
@@ -0,0 +1,10 @@
+
+--- 
+id: js
+description: |
+    This file is the standard Javascript template.
+    You can override this template in your own theme. 
+    You should always name the file "js.js".
+    It will be processed like any other template.
+    It will be exported to a file called "stacks.js" in your theme's folder.
+---
