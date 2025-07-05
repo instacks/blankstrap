382868acf39801c01847a6c4bf78b140846f2b7b
Jannis Rondorf <github@jrondorf.de>
2025-07-05 22:31:00 +0200

    delete old data

--- Blankstrap5pro.stack/accordion.stack/Contents/Resources/each.js
+++
@@ -1,7 +0,0 @@
-%[if %("%id=uid%" != "")%]%
-document.addEventListener('DOMContentLoaded',function(){
-    document.querySelector('#%id=uid%').querySelectorAll('.accordion-collapse').forEach(function(element){
-        element.setAttribute('data-bs-parent', '#%id=uid%');
-    });
-});
-%[endif]%
--- Blankstrap5pro.stack/sidebarnav.stack/Contents/Resources/each.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    blankstrap.initializeNavigation('#%[if %("%id=uid%" != "")%]%%id=uid%%[else]%%id%%[endif]%', '%id=fixed%');
-});
